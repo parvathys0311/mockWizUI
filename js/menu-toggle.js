6e7c81f5a86c671a58ddc6babd5043d5dc89abd1
Parvathy <parvathys0311@gmail.com>
2020-11-02 02:41:36 -0700

    mob friendly - tweaks

--- js/menu-toggle.js
+++ js/menu-toggle.js
@@ -9,19 +9,12 @@ console.log(menu)
 function menuToggle() {
   console.log('Menu is toggled');
   if (menu.classList.contains('active')){
-    console.log("if")
+    // console.log("if")
     menu.classList.remove('active')
   } else {
-    console.log('else')
-    
+    // console.log('else') 
     menu.classList.add('active');
   }
-  // This line of code runs only when `button` is clicked. 
-  // The 'visible' class will be removed/added (or toggled) each time this event handler is invoked
-  // menu.classList.toggle('visible');
-  // menu.classList.remove('item');
-  // const trythis = document.querySelector('nav ul li');
-  // console.log(trythis)
 }
 
 // The code in the `eventHandler` function (defined above) will be invoked each time `button` is clicked
